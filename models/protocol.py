b9be0fc3799fdc5dc168be623b7a3e1cdef88ce3
Rares Stanciu <rares.stanciu@outlook.com>
2022-03-23 12:19:04 +0200

    feat: unique protocol bytes id

--- models/protocol.py
+++ models/protocol.py
@@ -8,6 +8,6 @@ class Protocol(Base):
     __tablename__ = "protocols"
 
     id = Column(Integer, primary_key=True, default=1)
-    bytes_identifier = Column(Text, nullable=False)
+    bytes_identifier = Column(Text, nullable=False, unique=True)
     agent = Column(Text, nullable=False)
     coverage_ended_at = Column(TIMESTAMP)
