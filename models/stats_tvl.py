657efbb379dfafb9e436399df667a0c73afbe0f3
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-03-28 15:48:53 -0300

    remove unused import

--- models/stats_tvl.py
+++ models/stats_tvl.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from sqlalchemy import Column, Integer
 from sqlalchemy.dialects.postgresql import TIMESTAMP, NUMERIC, BIGINT
 
-import settings
 from models.base import Base
 
 
