7aa4f511b7cd3ae38ba01aca31a02f480db88064
Evert Kors <evert@hatching.io>
2022-03-30 07:19:17 +0200

    Use fixed apy

--- views/staking.py
+++ views/staking.py
@@ -19,21 +19,16 @@ def staking_positions(user):
     positions = [x.to_dict() for x in positions]
 
     # Compute USDC increment and updated balance
-    apy = indexer_data.apy
+    # TODO temporary fixed apy
+    apy = 0.15
 
     for pos in positions:
-        # TODO include counter in `StakingPositions` of how many times the position has been restaked
-        # TODO if pos.restaked == 0 && pos.id <= settings.LAST_POSITION_ID_FOR_15PERC_APY:
-        # TODO ^ to not show 15% APY when the users restake after 6 months
-        if pos.id <= LAST_POSITION_ID_FOR_15PERC_APY:
-            apy = 0.15
-
         pos["usdc_increment"] = calculate_increment(pos["usdc"], apy)
         pos["usdc"] = round(pos["usdc"] * indexer_data.balance_factor)
 
     return {
         "ok": True,
         "positions_usdc_last_updated": int(indexer_data.last_time.timestamp()),
-        "usdc_apy": round(indexer_data.apy * 100, 6), # Will not be affected by `LAST_POSITION_ID_FOR_15PERC_APY`
+        "usdc_apy": round(apy * 100, 6),
         "data": positions,
     }
