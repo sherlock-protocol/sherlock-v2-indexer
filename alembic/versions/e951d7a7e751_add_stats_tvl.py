6def6fcc288e06be0cd44f452989bc989baa8188
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-04-01 15:02:36 -0300

    chore: alembic merge migrations

---
+++ alembic/versions/e951d7a7e751_add_stats_tvl.py
@@ -0,0 +1,24 @@
+"""add stats_tvl
+
+Revision ID: e951d7a7e751
+Revises: 41805685552a
+Create Date: 2022-04-01 15:02:11.841261
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e951d7a7e751'
+down_revision = '41805685552a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
