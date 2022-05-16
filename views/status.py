21f08148907533c9f7735d8f30726af282f7c6bb
Rares Stanciu <rares.stanciu@outlook.com>
2022-05-16 10:47:37 +0300

    feat: add status page

---
+++ views/status.py
@@ -0,0 +1,21 @@
+from flask_app import app
+from models import IndexerState, Session, StakingPositionsMeta
+
+
+@app.route("/status")
+def status():
+    with Session() as s:
+        indexer_state = s.query(IndexerState).first()
+        staking_positions_meta = s.query(StakingPositionsMeta).first()
+
+    return {
+        "ok": True,
+        "data": {
+            "last_block": indexer_state.last_block,
+            "last_time": int(indexer_state.last_time.timestamp()),
+            "apy": indexer_state.apy,
+            "balance_factor": indexer_state.balance_factor,
+            "usdc_last_updated": int(staking_positions_meta.usdc_last_updated.timestamp()),
+            "usdc_last_updated_block": staking_positions_meta.usdc_last_updated_block,
+        },
+    }
