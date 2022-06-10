fde9a0b6a31cf905fd1e2b5fb1e09871fa8ba01a
Rares Stanciu <rares.stanciu@outlook.com>
2022-06-10 23:16:41 +0300

    chore: comments and disable double apy check

--- strategies/strategies.py
+++ strategies/strategies.py
@@ -58,10 +58,10 @@ class Strategy:
 
 
 class Strategies:
-    AAVE = Strategy(address="0xab9c505c21b59D7263e8409fB618262e9871b4D2", name="Aave")
-    COMPOUND = Strategy(address="0xab9c505c21b59D7263e8409fB618262e9871b4D2", name="Compound")
-    EULER = Strategy(address="0xab9c505c21b59D7263e8409fB618262e9871b4D2", name="Euler")
-    TRUEFI = Strategy(address="0xab9c505c21b59D7263e8409fB618262e9871b4D2", name="TrueFi")
-    MAPLE = Strategy(address="0xab9c505c21b59D7263e8409fB618262e9871b4D2", name="Maple")
+    AAVE = Strategy(address="0xE3C37e951F1404b162DFA71A13F0c99c9798Db82", name="Aave")
+    COMPOUND = Strategy(address="0x8AEA96da625791103a29a16C06c5cfC8B25f6832", name="Compound")
+    EULER = Strategy(address="0x9a902e8Aae5f1aB423c7aFB29C0Af50e0d3Fea7e", name="Euler")
+    TRUEFI = Strategy(address="0x1eC37c35BeE1b8b18fC01740db7750Cf93943254", name="TrueFi")
+    MAPLE = Strategy(address="0xfa01268bd200d0D0f13A6F9758Ba3C09F928E2f7", name="Maple")
 
     ALL = [AAVE, COMPOUND, EULER, TRUEFI, MAPLE]
