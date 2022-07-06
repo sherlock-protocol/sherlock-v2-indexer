12b87f01937db863967b15beb10784785cc4b893
Rares Stanciu <rares.stanciu@outlook.com>
2022-07-06 18:28:53 +0300

    feat: API for fetching strategy balances

--- views/__init__.py
+++ views/__init__.py
@@ -1,7 +1,8 @@
 from .chain import *  # noqa
+from .claims import *  # noqa
 from .fundraise import *  # noqa
 from .protocols import *  # noqa
 from .staking import *  # noqa
 from .stats import *  # noqa
-from .claims import *  # noqa
 from .status import *  # noqa
+from .strategies import *  # noqa
