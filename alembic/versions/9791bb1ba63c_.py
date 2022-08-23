0cefc483e7e32a6791b96e4cd6d4a9baf27700b7
Evert Kors <evert@hatching.io>
2022-08-23 22:07:19 +0200

    Run 'alembic merge heads'

---
+++ alembic/versions/9791bb1ba63c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9791bb1ba63c
+Revises: fe1a6b4ecd8a, 309b0f5de150
+Create Date: 2022-08-23 22:05:22.720565
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9791bb1ba63c'
+down_revision = ('fe1a6b4ecd8a', '309b0f5de150')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
